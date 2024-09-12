7cb8385b0280b0c9bde0b164d8e480cbf5e421b2
Ntombizilee <beingprivy@gmail.com>
2024-09-13 00:21:43 +0200

    Included the GitHub repo url.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Ntombizilee/" target="_blank">
+          Ntombizile Mabuso
+        </a>{" "}
+        and it is{""}
+        <a
+          href="https://github.com/Ntombizilee/final-react-project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
- 
