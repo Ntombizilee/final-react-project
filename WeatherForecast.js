6d3202274328a2ef5924d674c1668ccc1fd31026
Ntombizilee <beingprivy@gmail.com>
2024-09-22 00:04:15 +0200

    Added the API integration.

---
+++ WeatherForecast.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
